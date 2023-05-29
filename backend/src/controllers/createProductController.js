5b2ddfd7aded608399a6ce1b0d1a568e268f13e1
thiago-papim <thiago.fourp@gmail.com>
2023-05-29 15:03:34 -0300

    Requisito 4

--- backend/src/controllers/createProductController.js
+++ backend/src/controllers/createProductController.js
@@ -1,10 +1,25 @@
 const createProductService = require('../services/createProductServices');
 
+// const create = async (req, res) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res.status(404).json({ message: '"name" is required' });
+//   }
+//   const [[result]] = await createProductService.create(name);
+//   result.name = name;
+//   res.status(201).json(result);
+// };
 const create = async (req, res) => {
   const { name } = req.body;
-  const [[result]] = await createProductService.create(name);
-  result.name = name;
-  res.status(201).json(result);
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  const result = await createProductService.create(name);
+  if (result.type) {
+    return res.status(422).json({ message: result.message });
+  }
+  result.message[0][0].name = name;
+  res.status(201).json(result.message[0][0]);
 };
 
 module.exports = {
--- backend/src/services/createProductServices.js
+++ backend/src/services/createProductServices.js
@@ -2,7 +2,16 @@ const createProductModel = require('../models/createProductModel');
 
 const create = async (name) => {
   const result = await createProductModel.create(name);
-  return result;
+  if (name.length < 5) {
+    return {
+      type: 'error',
+      message: '"name" length must be at least 5 characters long',
+    };
+  }
+  return {
+    type: '',
+    message: result,
+  };
 };
 
 module.exports = {
